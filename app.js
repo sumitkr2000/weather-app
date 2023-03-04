9eb6609fd52b0e98acd78b0e16a4012c262d78f8
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-03-04 10:40:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const path = require("path");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render(__dirname + "/views/index.ejs");
+});
+
+app.post("/", (req, res) => {
+  const query = req.body.city;
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=cc6777de85e0c574383741f4825d2b1c&units=metric";
+
+  https.get(url, function (response) {
+    const code = response.statusCode;
+
+    if (code === 200) {
+      response.on("data", function (data) {
+        const weatherData = JSON.parse(data);
+        const temp = weatherData.main.temp;
+        const description = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+
+        res.render(__dirname + "/views/result.ejs", {
+          city: query,
+          temp: temp,
+          description: description,
+          image: icon,
+        });
+      });
+    } else {
+      res.render(__dirname + "/views/failure.ejs");
+    }
+  });
+});
+
+app.listen(4000, () => {
+  console.log("server started");
+});
